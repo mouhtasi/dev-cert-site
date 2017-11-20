3fad3df2dfe50104db068947d2fdab89f002774e
Imad Mouhtassem <imad@imadm.ca>
2017-11-19 22:31:40 -0500

    Get initial index view working

--- fizzbuzz/views.py
+++ fizzbuzz/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Topic
 
 
 def index(request):
-    return render(request, 'fizzbuzz/index.html')
+    topics = Topic.objects.all()
+    return render(request, 'fizzbuzz/index.html', {'topics': topics})
