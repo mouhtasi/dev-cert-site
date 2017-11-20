f8dd96f05ed0bb1612a6e237d4eadeab2cf3ca07
Imad Mouhtassem <imad@imadm.ca>
2017-11-19 21:58:32 -0500

    Created Topic, Lesson, and Test models

--- fizzbuzz/models.py
+++ fizzbuzz/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+
+class Test(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
