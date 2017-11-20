7191ad999e34173b5223ce3766d8b2747ddb487a
Imad Mouhtassem <imad@imadm.ca>
2017-11-19 23:45:58 -0500

    Get linking to lesson working. Add migration file for previous commit

--- fizzbuzz/urls.py
+++ fizzbuzz/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^lesson/(?P<topic>\w+)/$', views.lesson, name='lesson'),
 ]
--- fizzbuzz/views.py
+++ fizzbuzz/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render, get_object_or_404
+from .models import Lesson, Topic
 
 
 def index(request):
-    topics = Topic.objects.all()
-    return render(request, 'fizzbuzz/index.html', {'topics': topics})
+    lessons = Lesson.objects.all()
+    return render(request, 'fizzbuzz/index.html', {'lessons': lessons})
+
+
+def lesson(request, topic):
+    topic_id = Topic.objects.filter(name=topic)
+    lesson = get_object_or_404(Lesson, topic=topic_id)
+    return render(request, 'fizzbuzz/lesson.html', {'lesson': lesson})
