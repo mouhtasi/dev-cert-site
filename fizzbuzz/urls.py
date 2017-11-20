75ef73f1ec895271498021b563843f5690b4cf09
Imad Mouhtassem <imad@imadm.ca>
2017-11-19 23:59:52 -0500

    Added test url and template

--- fizzbuzz/urls.py
+++ fizzbuzz/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^lesson/(?P<topic>\w+)/$', views.lesson, name='lesson'),
+    url(r'^test/(?P<topic>\w+)/$', views.test, name='test'),
 ]
--- fizzbuzz/views.py
+++ fizzbuzz/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Lesson, Topic
+from .models import Lesson, Topic, Test
 
 
 def index(request):
@@ -11,3 +11,9 @@ def lesson(request, topic):
     topic_id = Topic.objects.filter(name=topic)
     lesson = get_object_or_404(Lesson, topic=topic_id)
     return render(request, 'fizzbuzz/lesson.html', {'lesson': lesson})
+
+
+def test(request, topic):
+    topic_id = Topic.objects.filter(name=topic)
+    test = get_object_or_404(Test, topic=topic_id)
+    return render(request, 'fizzbuzz/test.html', {'test': test})
