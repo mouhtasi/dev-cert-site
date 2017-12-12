abe5a1dcf35e37a193c27bbbd09df435cdecf5ae
Imad Mouhtassem <imad@imadm.ca>
2017-12-12 03:06:21 -0500

    Update the static url

--- fizzbuzzcert/settings.py
+++ fizzbuzzcert/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
     }
 }
 
+#AUTH_USER_MODEL = 'fizzbuzz.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -125,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/fizzbuzzcert/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
